18eadbaf005834008dbb3c0f97879ace3121a3a8
Maxwell Cabral <max@maxcabral.com>
2015-03-07 19:25:10 -0800

    Fix syntax errors, send reasonable error messages back to JS land

--- src/ios/CDVAudioSession.m
+++ src/ios/CDVAudioSession.m
@@ -8,6 +8,7 @@
     CDVPluginResult *pluginResult    = nil;
     NSString        *category        = nil;
     NSString        *categoryOptions = nil;
+    NSString        *errorMessage    = @"Invalid number of parameters";
 
     if (command.arguments.count == 1){
       category        = [command.arguments objectAtIndex:0];
@@ -34,20 +35,20 @@
         }
       
         if (setCategoryError != nil || !setSession){
-            NSString *errorMessage = setCategoryError != nil ? setCategoryError.description :
-                                                               @"AVAudioSession returned NO but did not provide an NSError";
+            errorMessage = setCategoryError != nil ? setCategoryError.description :
+                                                     @"AVAudioSession returned NO but did not provide an NSError";
             NSLog(@"Unable to set Audio Session values %@ to %@: %@",
                   category,
                   categoryOptions,
                   errorMessage);
         } else {
-          pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];      
+          pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK]; 
         }
     }
 
     // Assume we have an error if we didn't set a success message
     if (pluginResult == nil){
-      pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];      
+      pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:errorMessage];
     }
   
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
