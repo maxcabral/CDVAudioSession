bf2ff42562d44ffebd0d7b07819b8a027c79fda9
Maxwell Cabral <max@maxcabral.com>
2015-03-07 20:22:56 -0800

    Fix missing comma

--- www/CDVAudioSession.js
+++ www/CDVAudioSession.js
@@ -4,7 +4,7 @@ module.exports = {
 	setCategory : function (categoryString, success, fail) {
 		console.log('JS: setting audio session');
 		return cordova.exec(success, fail, "CDVAudioSession", "setCategoryAndOptions", [categoryString]);
-	}
+	},
 
 	setCategoryAndOptions : function (categoryString, optionString, success, fail) {
 		console.log('JS: setting audio session');
